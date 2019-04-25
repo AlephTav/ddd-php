9f4294d05294f13e1eb51caf5e793e485bc45978
Милимко Сергей <smilimko@gmail.com>
2019-04-25 13:15:16 +0500

    Rename properties of Money class.

--- src/Common/Model/Money.php
+++ src/Common/Model/Money.php
@@ -5,83 +5,83 @@ namespace AlephTools\DDD\Common\Model;
 use AlephTools\DDD\Common\Infrastructure\ValueObject;
 
 /**
- * @property-read string $value
- * @property-read Currency $code
+ * @property-read string $amount
+ * @property-read Currency $currency
  */
 class Money extends ValueObject
 {
-    private $value;
-    private $code;
+    private $amount;
+    private $currency;
 
     /**
      * Constructor.
      *
-     * @param array|string|int|float $value
-     * @param Currency|null $code
+     * @param array|string|int|float $amount
+     * @param Currency|null $currency
      */
-    public function __construct($value, Currency $code = null)
+    public function __construct($amount, Currency $currency = null)
     {
-        if (is_array($value)) {
-            parent::__construct($value);
+        if (is_array($amount)) {
+            parent::__construct($amount);
         } else {
             parent::__construct([
-                'value' => $value,
-                'code' => $code ?? Currency::USD()
+                'amount' => $amount,
+                'currency' => $currency ?? Currency::USD()
             ]);
         }
     }
 
-    public function cmp(string $value): int
+    public function cmp(string $amount): int
     {
-        return bccomp($this->value, $value, $this->code->getSubunits());
+        return bccomp($this->amount, $amount, $this->currency->getSubunits());
     }
 
-    public function add(string $value): Money
+    public function add(string $amount): Money
     {
-        return $this->op($value, 'bcadd');
+        return $this->op($amount, 'bcadd');
     }
 
-    public function sub(string $value): Money
+    public function sub(string $amount): Money
     {
-        return $this->op($value, 'bcsub');
+        return $this->op($amount, 'bcsub');
     }
 
-    public function mul(string $value): Money
+    public function mul(string $amount): Money
     {
-        return $this->op($value, 'bcmul');
+        return $this->op($amount, 'bcmul');
     }
 
-    public function div(string $value): Money
+    public function div(string $amount): Money
     {
-        return $this->op($value, 'bcdiv');
+        return $this->op($amount, 'bcdiv');
     }
 
-    private function op(string $value, string $operation): Money
+    private function op(string $amount, string $operation): Money
     {
-        $value = $operation($this->value, $value, $this->code->getSubunits());
-        return new Money($value, $this->code);
+        $amount = $operation($this->amount, $amount, $this->currency->getSubunits());
+        return new Money($amount, $this->currency);
     }
 
     public function toString(): string
     {
-        return $this->value;
+        return $this->amount;
     }
 
     //region Setters and Validators
 
-    private function setValue(?string $value): void
+    private function setAmount(?string $amount): void
     {
-        $this->value = $value ?? '0';
+        $this->amount = $amount ?? '0';
     }
 
-    private function setCode(?Currency $code): void
+    private function setCurrency(?Currency $currency): void
     {
-        $this->code = $code;
+        $this->currency = $currency;
     }
 
-    private function validateCode(): void
+    private function validateCurrency(): void
     {
-        $this->assertArgumentNotNull($this->code, 'Currency code must not be null.');
+        $this->assertArgumentNotNull($this->currency, 'Currency must not be null.');
     }
 
     //endregion
--- tests/Common/Model/MoneyTest.php
+++ tests/Common/Model/MoneyTest.php
@@ -13,33 +13,33 @@ class MoneyTest extends TestCase
     {
         $currency = new Money(123);
 
-        $this->assertSame('123', $currency->value);
-        $this->assertSame(Currency::USD(), $currency->code);
+        $this->assertSame('123', $currency->amount);
+        $this->assertSame(Currency::USD(), $currency->currency);
         $this->assertSame('123', $currency->toString());
 
         $currency = new Money(null, Currency::EUR());
 
-        $this->assertSame('0', $currency->value);
-        $this->assertSame(Currency::EUR(), $currency->code);
+        $this->assertSame('0', $currency->amount);
+        $this->assertSame(Currency::EUR(), $currency->currency);
         $this->assertSame('0', $currency->toString());
 
         $currency = new Money([
-            'value' => 111,
-            'code' => Currency::RUB()
+            'amount' => 111,
+            'currency' => Currency::RUB()
         ]);
 
-        $this->assertSame('111', $currency->value);
-        $this->assertSame(Currency::RUB(), $currency->code);
+        $this->assertSame('111', $currency->amount);
+        $this->assertSame(Currency::RUB(), $currency->currency);
         $this->assertSame('111', $currency->toString());
     }
 
     public function testCurrencyCodeValidation(): void
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Currency code must not be null.');
+        $this->expectExceptionMessage('Currency must not be null.');
 
         new Money([
-            'value' => 500
+            'amount' => 500
         ]);
     }
 
@@ -47,28 +47,28 @@ class MoneyTest extends TestCase
     {
         $currency = (new Money('13.67'))->add('12.56');
 
-        $this->assertSame('26.23', $currency->value);
+        $this->assertSame('26.23', $currency->amount);
     }
 
     public function testSub(): void
     {
         $currency = (new Money('11.04'))->sub('12.96');
 
-        $this->assertSame('-1.92', $currency->value);
+        $this->assertSame('-1.92', $currency->amount);
     }
 
     public function testMul(): void
     {
         $currency = (new Money('7.53'))->mul('17.79');
 
-        $this->assertSame('133.95', $currency->value);
+        $this->assertSame('133.95', $currency->amount);
     }
 
     public function testDiv(): void
     {
         $currency = (new Money('34.67'))->div('5.01');
 
-        $this->assertSame('6.92', $currency->value);
+        $this->assertSame('6.92', $currency->amount);
     }
 
     public function testCmp(): void

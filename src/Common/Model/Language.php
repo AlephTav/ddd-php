32d7f45f762af4728f8b532cb9a901d12cf21dbf
Egor Talantsev <spyric@ananas-web.ru>
2022-04-01 16:54:47 +0500

    Add KZ, CS, HU to 1.9.X versions

--- src/Common/Model/Language.php
+++ src/Common/Model/Language.php
@@ -11,7 +11,10 @@ use AlephTools\DDD\Common\Infrastructure\Enums\AbstractEnum;
  * @method static static EN(string $method = null)
  * @method static static DE(string $method = null)
  * @method static static VI(string $method = null)
+ * @method static static KZ(string $method = null)
  * @method static static UZ(string $method = null)
+ * @method static static CS(string $method = null)
+ * @method static static HU(string $method = null)
  */
 class Language extends AbstractEnum
 {
@@ -21,7 +24,10 @@ class Language extends AbstractEnum
     private const EN = 'en_US';
     private const DE = 'de_DE';
     private const VI = 'vi_VN';
+    private const KZ = 'kk_KZ';
     private const UZ = 'uz_UZ';
+    private const CS = 'cs_CZ';
+    private const HU = 'hu_HU';
 
     private string $locale;
 

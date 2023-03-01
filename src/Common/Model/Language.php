88e1b223ef53861cfc432ce96004a34ce2d21df8
Oleg Karavaev <oleg.karavaev@ananas-web.ru>
2023-03-01 12:00:43 +0500

    fix dialects

--- src/Common/Model/Language.php
+++ src/Common/Model/Language.php
@@ -19,23 +19,23 @@ use AlephTools\DDD\Common\Infrastructure\Enums\AbstractEnum;
  * @method static static HU(string $method = null)
  * @method static static ID(string $method = null)
  * @method static static TJ(string $method = null)
- * @method static static GB(string $method = null)
- * @method static static KE(string $method = null)
- * @method static static NG(string $method = null)
- * @method static static EG(string $method = null)
- * @method static static AE(string $method = null)
- * @method static static SA(string $method = null)
- * @method static static KW(string $method = null)
- * @method static static BH(string $method = null)
- * @method static static AR(string $method = null)
- * @method static static VE(string $method = null)
- * @method static static MX(string $method = null)
- * @method static static BO(string $method = null)
- * @method static static CL(string $method = null)
- * @method static static BR(string $method = null)
- * @method static static PT(string $method = null)
- * @method static static EE(string $method = null)
- * @method static static LV(string $method = null)
+ * @method static static EN_GB(string $method = null)
+ * @method static static EN_KE(string $method = null)
+ * @method static static EN_NG(string $method = null)
+ * @method static static AR_EG(string $method = null)
+ * @method static static AR_AE(string $method = null)
+ * @method static static AR_SA(string $method = null)
+ * @method static static AR_KW(string $method = null)
+ * @method static static AR_BH(string $method = null)
+ * @method static static ES_AR(string $method = null)
+ * @method static static ES_VE(string $method = null)
+ * @method static static ES_MX(string $method = null)
+ * @method static static ES_BO(string $method = null)
+ * @method static static ES_CL(string $method = null)
+ * @method static static PT_BR(string $method = null)
+ * @method static static PT_PT(string $method = null)
+ * @method static static ET_EE(string $method = null)
+ * @method static static LV_LV(string $method = null)
  */
 class Language extends AbstractEnum
 {
@@ -51,23 +51,23 @@ class Language extends AbstractEnum
     private const HU = 'hu_HU';
     private const ID = 'id_ID';
     private const TJ = 'tg_TJ';
-    private const GB = 'en_GB';
-    private const KE = 'en_KE';
-    private const NG = 'en_NG';
-    private const EG = 'ar_EG';
-    private const AE = 'ar_AE';
-    private const SA = 'ar_SA';
-    private const KW = 'ar_KW';
-    private const BH = 'ar_BH';
-    private const AR = 'es_AR';
-    private const VE = 'es_VE';
-    private const MX = 'es_MX';
-    private const BO = 'es_BO';
-    private const CL = 'es_CL';
-    private const BR = 'pt_BR';
-    private const PT = 'pt_PT';
-    private const EE = 'et_EE';
-    private const LV = 'lv_LV';
+    private const EN_GB = 'en_GB';
+    private const EN_KE = 'en_KE';
+    private const EN_NG = 'en_NG';
+    private const AR_EG = 'ar_EG';
+    private const AR_AE = 'ar_AE';
+    private const AR_SA = 'ar_SA';
+    private const AR_KW = 'ar_KW';
+    private const AR_BH = 'ar_BH';
+    private const ES_AR = 'es_AR';
+    private const ES_VE = 'es_VE';
+    private const ES_MX = 'es_MX';
+    private const ES_BO = 'es_BO';
+    private const ES_CL = 'es_CL';
+    private const PT_BR = 'pt_BR';
+    private const PT_PT = 'pt_PT';
+    private const ET_EE = 'et_EE';
+    private const LV_LV = 'lv_LV';
 
     private string $locale;
 

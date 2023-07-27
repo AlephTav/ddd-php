8821691a248cf4b6970d38a197d2ce9b0ff6b6c4
Sergey Milimko <4lephtav@gmail.com>
2023-07-27 19:23:08 +0500

    Add batch method to Async

--- src/Common/Infrastructure/Async.php
+++ src/Common/Infrastructure/Async.php
@@ -8,7 +8,11 @@ interface Async
 {
     /**
      * Asynchronously executes (in background) a callback.
-     *
      */
     public function run(mixed $callback, array $params = []): void;
+
+    /**
+     * Asynchronously executes (in background) callbacks.
+     */
+    public function runBatch(mixed $callback, array $params = []): void;
 }

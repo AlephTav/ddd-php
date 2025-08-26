663810b3c46b826834bb422bd556e07bd9036761
Алексей Макаров <alexey.makarov@ananas-web.ru>
2025-08-26 15:17:54 +0200

    #TWIN-12015 Добавить на платформу китайский и армянский языки

--- src/Common/Model/Language.php
+++ src/Common/Model/Language.php
@@ -19,6 +19,7 @@ use AlephTools\DDD\Common\Infrastructure\Enums\AbstractEnum;
  * @method static static HU(string $method = null)
  * @method static static ID(string $method = null)
  * @method static static TJ(string $method = null)
+ * @method static static HY(string $method = null)
  * @method static static EN_GB(string $method = null)
  * @method static static EN_KE(string $method = null)
  * @method static static EN_NG(string $method = null)
@@ -51,6 +52,7 @@ use AlephTools\DDD\Common\Infrastructure\Enums\AbstractEnum;
  * @method static static RO_RO(string $method = null)
  * @method static static ES_CO(string $method = null)
  * @method static static FIL_PH(string $method = null)
+ * @method static static ZH_CN(string $method = null)
  */
 class Language extends AbstractEnum
 {
@@ -67,6 +69,7 @@ class Language extends AbstractEnum
     private const ID = ['id_ID', 'bahasa indonesia'];
     private const TJ = ['tg_TJ', 'тоҷикӣ'];
     private const KY = ['ky_KG', 'кыргызча'];
+    private const HY = ['hy_HY', 'հայերէն'];
     private const EN_GB = ['en_GB', 'english (GB)'];
     private const EN_KE = ['en_KE', 'english (KE)'];
     private const EN_NG = ['en_NG', 'english (NG)'];
@@ -102,6 +105,7 @@ class Language extends AbstractEnum
     private const SR_RS = ['sr-RS', 'Srpski'];
     private const EL_GR = ['el-GR', 'Ελληνικά'];
     private const IW_IL = ['iw-IL', 'עִבְרִית'];
+    private const ZH_CN = ['zh-CN', '汉语'];
 
     private string $locale;
     private string $name;
